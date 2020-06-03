449c5edf82ebc753b4a7e129f1c7697f4c632f25
Namibia Torres <NamibiaLT@gmail.com>
2020-06-03 13:28:09 -0700

    add initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('htmlGenerator', { horizontalLayout: 'full' })
+    )
+);
+
+// if (files.directoryExists('.git')) {
+//     console.log(chalk.red('Already a Git repository!'));
+//     process.exit();
+// }
+
+
+
+const run = async () => {
+    const credentials = await inquirer.askGithubCredentials();
+    console.log(credentials);
+};
+
+run();
